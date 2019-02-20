9da912e00b7f0a465b128e12b4127a0553f26966
clayton j mosher <mosherclayton@gmail.com>
2019-02-20 03:04:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,54 +25,54 @@ function viewCart() {
     return console.log("Your shopping cart is empty.");
   }
 
-  const itemsAndPrices = []
+  const itemsAndPrices = [];
 
   for (let i = 0; i < l; i++) {
-    let itemAndPrice = cart[i]
-    let item = Object.keys(itemAndPrice)[0]
-    let price = itemAndPrice[item]
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
 
-    itemsAndPrices.push(`${item} at \$${price}`)
+    itemsAndPrices.push(`${item} at \$${price}`);
   }
 
-  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
+  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`);
 }
 
 function removeFromCart(item) {
-  let itemInCart = false
+  let itemInCart = false;
 
   for (let i = 0, l = cart.length; i < l; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      itemInCart = true
-      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
     }
   }
 
   if (!itemInCart) {
-    console.log("That item is not in your cart.")
+    console.log("That item is not in your cart.");
   }
 
-  return cart
+  return cart;
 }
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return console.log("We don't have a credit card on file for you to place your order.")
+    return console.log("We don't have a credit card on file for you to place your order.");
   }
 
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
 
-  cart = []
+  cart = [];
 }
 
 function total() {
-  let t = 0
+  let t = 0;
 
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
-      t += cart[i][item]
+      t += cart[i][item];
     }
   }
 
-  return t
+  return t;
 }
